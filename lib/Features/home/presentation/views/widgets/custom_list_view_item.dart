b67f50757b7e00e6a30c3ab34163dd6365b07ef7
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-12-13 00:59:46 +0300

    add featured_book_list_view.dart

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -6,20 +6,17 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*.3,
-      child: AspectRatio(
-        aspectRatio: 2.7/4,
-        child: Container(
-          height: MediaQuery.of(context).size.height*.25,
-          width: 100,
-          decoration:  BoxDecoration(
-            borderRadius:BorderRadius.circular(16),
-            color: Colors.red,
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.test),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.7/4,
+      child: Container(
+        height: MediaQuery.of(context).size.height*.25,
+        width: 100,
+        decoration:  BoxDecoration(
+          borderRadius:BorderRadius.circular(16),
+          color: Colors.red,
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.test),
           ),
         ),
       ),
