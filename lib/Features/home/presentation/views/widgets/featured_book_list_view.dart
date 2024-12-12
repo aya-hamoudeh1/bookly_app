f0d59a874596efc754cb39843defb70ab8b2d566
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-12-13 01:01:32 +0300

    add featured_book_list_view.dart to home

--- lib/Features/home/presentation/views/widgets/featured_book_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_book_list_view.dart
@@ -13,7 +13,7 @@ class FeaturedBookListView extends StatelessWidget {
         itemBuilder: (context, index) {
           return const Padding(
             padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: CustomListViewItem(),
+            child: FeaturedListViewItem(),
           );
         },
       ),
