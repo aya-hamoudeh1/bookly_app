36360c30f98875411870056a0425cdb069bb9456
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-12-13 17:40:35 +0300

    best seller list view item part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Core/utils/assets.dart';
 import 'package:bookly_app/Core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
@@ -18,11 +19,48 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(
             height: 50,
           ),
-          Text("Best Seller",
-          style: Styles.titleMedium,),
+          Text(
+            "Best Seller",
+            style: Styles.titleMedium,
+          ),
+          BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+        AspectRatio(
+        aspectRatio: 2.5/4,
+        child: Container(
+          height: MediaQuery.of(context).size.height*.25,
+          width: 100,
+          decoration:  BoxDecoration(
+            borderRadius:BorderRadius.circular(8),
+            color: Colors.red,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.test),
+            ),
+          ),
+        ),
+      ),
+          const Column(
+            children: [
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
